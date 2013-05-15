bcb0ecc4692a98a123d15465808e77cfee8db872
circuschaser <rob@circuschaser.com>
2013-05-15 18:30:23 -0400

    add user

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,7 +8,7 @@ describe "Static pages" do
     before { visit root_path }
     it { should have_selector('h1', text: 'Sample App') }
 
-    it { should have_selector('title', text: full_title('')) }
+    it { should have_selector('title', text: 'Rails Tutorial') }
     
     it { should_not have_selector('title', text: '| Home') }
   end
